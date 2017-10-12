d63f14d6ecba797522a512e34623e39888198fdc
pflemes <pedro_lemes@msn.com>
2017-10-12 12:32:15 -0300

    Criação cppConsole

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Adivar
+ *
+ * Created on 6 de Outubro de 2017, 00:21
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Hello World";
+
+    return 0;
+}
+
